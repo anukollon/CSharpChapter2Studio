a11a485e6f1b44ed27d736bc5e12dd198e41cc0f
anukollon <68405755+anukollon@users.noreply.github.com>
2020-09-29 22:50:18 -0500

    Chapter 2 studio

---
+++ Circle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chapter2Studio
+{
+    class Circle
+    {
+        public static double areaOfCircle(double radius)
+        {
+             return (Math.PI * radius * radius);
+        }
+
+        public static double getDiameter(double radius)
+        {
+            return 2 * radius;
+        }
+
+        public static double getCircumference(double radius)
+        {
+            return 2 * Math.PI * radius;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Chapter2Studio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            double radius = -1;
+            double gallonsOfGas;
+            string mpg;
+            double mpgDouble = -1;
+
+            Console.WriteLine("Enter a radius: ");
+            input = Console.ReadLine();
+            try
+            {
+                radius = double.Parse(input);
+            }
+            catch
+            {
+                Console.WriteLine("Invalid Entry!");
+                Environment.Exit(0);
+            }
+            
+            while(radius <= 0)
+            {
+                Console.WriteLine("Enter a radius: ");
+                input = Console.ReadLine();
+                radius = double.Parse(input);
+            }
+
+            Console.WriteLine("The area of a circle of radius " + radius + " is: " + Math.Round(Circle.areaOfCircle(radius),3));
+
+            Console.WriteLine("The diamater of a circle of radius " + radius + " is: " + Math.Round(Circle.getDiameter(radius),3));
+
+            Console.WriteLine("What is the MPG of your car?");
+            mpg = Console.ReadLine();
+            try
+            {
+                mpgDouble = double.Parse(mpg);
+                if(mpgDouble <= 0)
+                {
+                    Console.WriteLine("Invalid Entry!");
+                    Environment.Exit(0);
+                }
+            }
+            catch 
+            {
+                Console.WriteLine("Invalid Entry!");
+                Environment.Exit(0);
+            }
+
+            gallonsOfGas = Circle.getCircumference(radius) / mpgDouble;
+
+            Console.WriteLine("Gallons of gas used to go around the circle " + Math.Round(gallonsOfGas,3));
+        }
+    }
+}
